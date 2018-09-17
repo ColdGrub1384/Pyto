b8ccd30aeef80fa7933a0365546ee4b9a482ae4c
Adrian Labbé <adrilabbelol@gmail.com>
2018-09-17 16:05:10 -0300

    Share extension

---
+++ Pods/Target Support Files/Pods-Pyto Share/Pods-Pyto Share-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pyto_Share : NSObject
+@end
+@implementation PodsDummy_Pods_Pyto_Share
+@end
---
+++ PytoClassesShareExtension.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains classes from the share extension used by Pyto.
+"""
+
+from rubicon.objc import *
+
+PyMainThread = ObjCClass("Pyto_Share.PyMainThread")
+
+PyInputHelper = ObjCClass("Pyto_Share.PyInputHelper")
+PyOutputHelper = ObjCClass("Pyto_Share.PyOutputHelper")
+PySharingHelper = ObjCClass("Pyto_Share.PySharingHelper")
+FilePicker = ObjCClass("Pyto_Share.PyFilePicker")
+Alert = ObjCClass("Pyto_Share.PyAlert")
+PyContentViewController = ObjCClass("Pyto_Share.PyContentViewController")
