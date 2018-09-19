b04a4e7c811578465116d824f131835f0ab3903c
Adrian Labbé <adrilabbelol@gmail.com>
2018-09-19 16:44:40 -0300

    Support Open In Place

--- Pods/Target Support Files/SavannaKit-iOS11.0/SavannaKit-iOS11.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SavannaKit_iOS11_0 : NSObject
-@end
-@implementation PodsDummy_SavannaKit_iOS11_0
-@end
--- Pods/Target Support Files/SourceEditor-iOS12.0/SourceEditor-iOS12.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SourceEditor_iOS12_0 : NSObject
-@end
-@implementation PodsDummy_SourceEditor_iOS12_0
-@end
