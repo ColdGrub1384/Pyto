9179c506ac4317ac4296e5c86ac5950b06da2401
Adrian Labbé <adrilabbelol@gmail.com>
2018-09-21 22:18:07 -0300

    Dissociate Python scripts from the app

--- Pyto/Singletons/AppDelegate.swift
+++ Pyto/Singletons/AppDelegate.swift
@@ -17,7 +17,7 @@ import StoreKit
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
                 
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = DocumentBrowserViewController()
+        window?.rootViewController = DocumentBrowserViewController(forOpeningFilesWithContentTypes: ["public.python-script"])
         window?.accessibilityIgnoresInvertColors = true
         window?.makeKeyAndVisible()
         
