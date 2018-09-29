bd6a3ea1a1dd6773ca65e14c3e09faa801cebcca
Adrian Labbé <adrilabbelol@gmail.com>
2018-09-29 19:57:44 -0300

    Add more packages

---
+++ site-packages/numpy/f2py/__init__.py
@@ -0,0 +1,48 @@
+from __future__ import division, absolute_import, print_function
+
+__all__ = ['run_main', 'compile', 'f2py_testing']
+
+import os
+import sys
+import subprocess
+
+from . import f2py2e
+from . import f2py_testing
+from . import diagnose
+
+from .info import __doc__
+
+run_main = f2py2e.run_main
+main = f2py2e.main
+
+def compile(source,
+            modulename = 'untitled',
+            extra_args = '',
+            verbose = 1,
+            source_fn = None
+            ):
+    ''' Build extension module from processing source with f2py.
+    Read the source of this function for more information.
+    '''
+    from numpy.distutils.exec_command import exec_command
+    import tempfile
+    if source_fn is None:
+        fname = os.path.join(tempfile.mktemp()+'.f')
+    else:
+        fname = source_fn
+
+    f = open(fname, 'w')
+    f.write(source)
+    f.close()
+
+    args = ' -c -m %s %s %s'%(modulename, fname, extra_args)
+    c = '%s -c "import numpy.f2py as f2py2e;f2py2e.main()" %s' %(sys.executable, args)
+    s, o = exec_command(c)
+    if source_fn is None:
+        try: os.remove(fname)
+        except OSError: pass
+    return s
+
+from numpy.testing import Tester
+test = Tester().test
+bench = Tester().bench
---
+++ site-packages/numpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+short_version = '1.8.0'
+version = '1.8.0'
+full_version = '1.8.0'
+git_revision = 'a60b3901cd635d28bef8328e83bafd35ce631e08'
+release = True
+
+if not release:
+    version = full_version
