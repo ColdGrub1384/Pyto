f62f5a81089a5850a245082dddbd090ea0ec711e
Adrian Labbé <adrilabbelol@gmail.com>
2018-11-17 10:50:59 -0300

    Add Documents/modules to path

--- Pyto/Singletons/AppDelegate.swift
+++ Pyto/Singletons/AppDelegate.swift
@@ -28,6 +28,11 @@ import SafariServices
         
         window?.accessibilityIgnoresInvertColors = true
         
+        let modulesURL = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask)[0].appendingPathComponent("modules")
+        if !FileManager.default.fileExists(atPath: modulesURL.path) {
+            try? FileManager.default.createDirectory(at: modulesURL, withIntermediateDirectories: false, attributes: nil)
+        }
+        
         ReviewHelper.shared.launches += 1
         ReviewHelper.shared.requestReview()
         if ReviewHelper.shared.launches == 5 && !UserDefaults.standard.bool(forKey: "pisth") {
