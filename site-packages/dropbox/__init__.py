9e36cd4cd3e125b459477ea7b8c97d883cd97cdf
Adrian Labbé <adrilabbelol@gmail.com>
2018-11-23 20:37:34 -0300

    Remove third party packages
    They were not working lel

--- site-packages/dropbox/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from .dropbox import __version__, Dropbox, DropboxTeam, create_session
-from .oauth import DropboxOAuth2Flow, DropboxOAuth2FlowNoRedirect
-
-# Compatibility with the deprecated v1 client.
-from . import client, rest, session
