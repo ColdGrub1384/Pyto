9399c562baf025e02fe1ef47360a20dbb449ceda
ColdGrub1384 <adrilabbelol@gmail.com>
2019-01-23 10:16:05 -0300

    Replace tabs with double spaces

---
+++ Pyto/Extensions/NSRange.swift
@@ -0,0 +1,22 @@
+//
+//  NSRange.swift
+//  Pyto
+//
+//  Created by Adrian Labbé on 1/23/19.
+//  Copyright © 2019 Adrian Labbé. All rights reserved.
+//
+
+import UIKit
+
+// Taken from https://stackoverflow.com/a/46268900/7515957
+
+extension NSRange {
+    
+    func toTextRange(textInput:UITextInput) -> UITextRange? {
+        if let rangeStart = textInput.position(from: textInput.beginningOfDocument, offset: location),
+            let rangeEnd = textInput.position(from: rangeStart, offset: length) {
+            return textInput.textRange(from: rangeStart, to: rangeEnd)
+        }
+        return nil
+    }
+}
