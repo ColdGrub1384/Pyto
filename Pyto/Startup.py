91c481ba974a8faa8eb4b22643d6b7dfdfe2aa68
ColdGrub1384 <adrilabbelol@gmail.com>
2019-02-07 23:49:14 -0300

    Fix startup

--- Pyto/Startup.py
+++ Pyto/Startup.py
@@ -1,39 +1,45 @@
-__builtins__.iOS = 'iOS'
-__builtins__.macOS = 'macOS'
-__builtins__.__platform__ = iOS
+__builtins__.iOS = "iOS"
+__builtins__.macOS = "macOS"
+__builtins__.__platform__ = __builtins__.iOS
 
-__builtins__.widget = 'widget'
-__builtins__.app = 'app'
-__builtins__.__host__ = None
+__builtins__.widget = "widget"
+__builtins__.app = "app"
+__builtins__.__host__ = app
 
 import sys
-import console as Pyto
+import os
+
+sys.path.insert(0, os.path.expanduser("~/Library/pylib"))
+sys.path.insert(0, os.path.expanduser("~/Documents"))
+sys.path.insert(0, os.path.expanduser("~/Documents/modules"))
+
+import io
+import console
 import code
 import pyto
 from importlib.machinery import SourceFileLoader
 import importlib
 import threading
-from outputredirector import *
-import io
-import traceback
+from time import sleep
+from outputredirector import Reader
+from extensionsimporter import *
 
 pyto.Python.shared.version = sys.version
 
 # MARK: - Input
 
 def askForInput(prompt=None):
-    if (threading.currentThread() in Pyto.ignoredThreads):
+    if (threading.currentThread() in console.ignoredThreads):
         return ""
     else:
-        return Pyto.input(prompt)
-
+        return console.input(prompt)
 
 __builtins__.input = askForInput
 
 # MARK: - Output
 
 def read(text):
-    Pyto.print(text, end="")
+    console.print(text, end="")
 
 standardOutput = Reader(read)
 standardOutput._buffer = io.BufferedWriter(standardOutput)
@@ -44,28 +50,24 @@ standardError._buffer = io.BufferedWriter(standardError)
 sys.stdout = standardOutput
 sys.stderr = standardError
 
-# MARK: - REPL
+# MARK: - Modules
 
-interact = code.interact
-def newInteract():
-    pyto.Python.shared.isREPLRunning = True
-    interact()
-code.interact = newInteract
+sys.meta_path.append(NumpyImporter())
+sys.meta_path.append(MatplotlibImporter())
 
 # MARK: - Create a Selector without class.
 
 __builtins__.Selector = pyto.PySelector.makeSelector
 __builtins__.Target = pyto.SelectorTarget.shared
+
+# MARK: - Deprecations
+
 __builtins__.deprecated = ["runAsync", "runSync", "generalPasteboard", "setString", "setStrings", "setImage", "setImages", "setURL", "setURLs", "showViewController", "closeViewController", "mainLoop", "openURL", "shareItems", "pickDocumentsWithFilePicker"]
 
 # MARK: - Run script
 
-script = "%@"
-
-print("Will run "+script+"\n")
-
-try:
-    SourceFileLoader("main", script).load_module()
-except Exception as e:
-    ex_type, ex, tb = sys.exc_info()
-    traceback.print_tb(tb)
+while True:
+    try:
+        SourceFileLoader("main", "%@").load_module()
+    except Exception as e:
+        print(e)
