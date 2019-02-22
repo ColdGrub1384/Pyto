916104b000e2e019770533a9fd86a6d4b82618f6
ColdGrub1384 <adrilabbelol@gmail.com>
2019-02-22 20:35:04 -0300

    Add acknowledgments on macOS app

--- Pyto Mac/View controllers/DocumentationViewController.swift
+++ Pyto Mac/View controllers/DocumentationViewController.swift
@@ -10,67 +10,9 @@ import Cocoa
 import WebKit
 
 /// A View controller containing documentation.
-class DocumentationViewController: NSViewController, WKNavigationDelegate {
+class DocumentationViewController: WebViewController {
     
-    /// The Web view containing documentation.
-    @IBOutlet weak var webView: WKWebView!
-    
-    /// Button for going back.
-    var backButton: NSButton?
-    
-    /// Button for going forward.
-    var forwardButton: NSButton?
-    
-    /// Goes back.
-    @objc func goBack(_ sender: Any) {
-        webView.goBack()
-    }
-    
-    /// Goes forward.
-    @objc func goForward(_ sender: Any) {
-        webView.goForward()
-    }
-    
-    // MARK: - View controller
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let docsURL = Bundle.main.url(forResource: "docs", withExtension: nil) {
-            webView.loadFileURL(docsURL.appendingPathComponent("index.html"), allowingReadAccessTo: docsURL)
-        }
-        
-        webView.navigationDelegate = self
-    }
-    
-    override func viewWillAppear() {
-        super.viewWillAppear()
-        
-        backButton = view.window?.toolbar?.items[0].view as? NSButton
-        forwardButton = view.window?.toolbar?.items[1].view as? NSButton
-        
-        backButton?.target = self
-        backButton?.action = #selector(goBack(_:))
-        
-        forwardButton?.target = self
-        forwardButton?.action = #selector(goForward(_:))
-    }
-    
-    // MARK: - Navigation delegate
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        
-        backButton?.isEnabled = webView.canGoBack
-        forwardButton?.isEnabled = webView.canGoForward
-    }
-    
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        
-        if let url = navigationAction.request.url, !url.isFileURL {
-            NSWorkspace.shared.open(url)
-            decisionHandler(.cancel)
-        } else {
-            decisionHandler(.allow)
-        }
+    override var fileURL: URL? {
+        return Bundle.main.url(forResource: "docs/index", withExtension: "html")
     }
 }
