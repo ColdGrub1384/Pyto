9349f307fe0ab35e09ae92fd127f92a99157ddb9
ColdGrub1384 <adrilabbelol@gmail.com>
2019-03-02 15:26:31 -0300

    Update definition of platforms

--- PytoCore/Startup.py
+++ PytoCore/Startup.py
@@ -1,6 +1,6 @@
 __builtins__.iOS = 'iOS'
 __builtins__.macOS = 'macOS'
-__builtins__.__platform__ = iOS
+__builtins__.__platform__ = __builtins__.iOS
 
 __builtins__.widget = 'widget'
 __builtins__.app = 'app'
