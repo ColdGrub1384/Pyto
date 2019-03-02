5874a359bd89590650e9b5a6035248f59594c196
cclauss <cclauss@me.com>
2019-03-02 06:26:10 +0100

    Use str.strip() to remove leading or trailing whitespace
    Also print with an f”string”.  https://realpython.com/python-f-strings/

--- Python App/Python App/app/main.py
+++ Python App/Python App/app/main.py
@@ -2,7 +2,7 @@ from time import sleep
 
 # Code here
 
-name = input("What's your name? ")
-print("Hello "+name+"!")
+name = input("What's your name? ").strip()
+print(f"Hello {name}!")
 
 sleep(1)  # After 1 second, the app will exit.
