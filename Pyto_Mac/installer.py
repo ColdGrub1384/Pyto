aa29bf8d6fed60accb7be5c85cf0e57720f8629c
ColdGrub1384 <adrilabbelol@gmail.com>
2019-05-04 12:00:50 -0400

    Make pip work for Python 2

--- Pyto_Mac/installer.py
+++ Pyto_Mac/installer.py
@@ -5,10 +5,14 @@ After executing this script, type arguments you want to pass to pip.
 """
 
 from pip._internal import main
+from sys import version
 import os
 
 os.environ["COLUMNS"] = "100" # Sets terminal's horizontal size
 
-args = input("pip ") # Asks for arguments
+if version[0] > "2":
+    args = input("pip ") # Asks for arguments
+else:
+    args = raw_input("pip ") # Asks for arguments
 
 main(args.split(" ")) # Runs pip with typed arguments
