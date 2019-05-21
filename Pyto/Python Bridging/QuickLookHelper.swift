f5041452066787d4f65b648ee7fd963f76541a22
ColdGrub1384 <adrilabbelol@gmail.com>
2019-05-21 12:46:25 -0400

    Show Matplotlib figures with QuickLook if console is on bottom

--- Pyto/Python Bridging/QuickLookHelper.swift
+++ Pyto/Python Bridging/QuickLookHelper.swift
@@ -50,22 +50,108 @@ fileprivate class ImageAttachment: NSTextAttachment {
 }
 
 /// A class that helps Python scripts to preview a file.
-@objc class QuickLookHelper: NSObject {
+@objc class QuickLookHelper: NSObject, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
+    
+    private var controller: QLPreviewController?
+    
+    private var filePaths: [String] {
+        didSet {
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.1) {
+                self.controller?.reloadData()
+            }
+        }
+    }
+    
+    private init(filePaths: [String]) {
+        self.filePaths = filePaths
+    }
+    
+    /// The data source of the currently displaying controller.
+    static var visible: QuickLookHelper?
     
     /// Previews given file.
     ///
     /// - Parameters:
     ///     - file: The path of the file.
     @objc static func previewFile(_ file: String) {
-        
         DispatchQueue.main.async {
-            if let image = UIImage(contentsOfFile: file) {
+            
+            func showOnConsole() {
+                guard let image = UIImage(contentsOfFile: file) else {
+                    return
+                }
                 let attachment = ImageAttachment()
                 attachment.image = image
                 let attrString = NSMutableAttributedString(attributedString: NSAttributedString(attachment: attachment))
                 attrString.append(NSAttributedString(string: "\n"))
                 ConsoleViewController.visible.textView.textStorage.insert(attrString, at: ConsoleViewController.visible.textView.offset(from: ConsoleViewController.visible.textView.endOfDocument, to: ConsoleViewController.visible.textView.endOfDocument))
             }
+            
+            #if MAIN
+            if !EditorSplitViewController.shouldShowConsoleAtBottom {
+                showOnConsole()
+            } else {
+                DispatchQueue.main.async {
+                    guard visible == nil else {
+                        visible?.filePaths.append(file)
+                        return
+                    }
+                    
+                    for fileURL in ((try? FileManager.default.contentsOfDirectory(at: URL(fileURLWithPath: NSTemporaryDirectory()), includingPropertiesForKeys: nil, options: .skipsHiddenFiles)) ?? []) {
+                        if fileURL.path != file {
+                            try? FileManager.default.removeItem(at: fileURL)
+                        }
+                    }
+                    
+                    let dataSource = QuickLookHelper(filePaths: [file])
+                    
+                    let controller = QLPreviewController()
+                    controller.dataSource = dataSource
+                    controller.delegate = dataSource
+                    
+                    #if MAIN || WIDGET
+                    var vc: UIViewController?
+                    if ConsoleViewController.visible.view.window != nil {
+                        vc = ConsoleViewController.visible
+                    }
+                    #if MAIN
+                    if vc == nil, let browser = DocumentBrowserViewController.visible {
+                        vc = browser
+                    }
+                    
+                    if vc == nil {
+                        return
+                    }
+                    #endif
+                    #else
+                    let vc = UIApplication.shared.keyWindow?.topViewController
+                    #endif
+                    
+                    visible = dataSource
+                    
+                    vc?.present(controller, animated: true, completion: nil)
+                }
+            }
+            #else
+            showConsole()
+            #endif
         }
+    }
+    
+    // MARK: - Preview controller data source
+    
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        self.controller = controller
+        return filePaths.count
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        return URL(fileURLWithPath: filePaths[index]) as QLPreviewItem
+    }
+    
+    // MARK: - Preview controller delegate
+    
+    func previewControllerDidDismiss(_ controller: QLPreviewController) {
+        QuickLookHelper.visible = nil
     }
 }
