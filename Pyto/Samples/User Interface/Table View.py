c72dfb7e586c8c42a2f172a9469d4ade8d744a72
Adrian Labbé <adrilabbelol@gmail.com>
2019-08-18 19:39:41 -0400

    Fix Table View sample

--- Pyto/Samples/User Interface/Table View.py
+++ Pyto/Samples/User Interface/Table View.py
@@ -55,57 +55,57 @@ all_items = {
 }
 
 def selected(section, cell_index):
-  section.table_view.deselect_row()
-  item = all_items[section][cell_index]
+    section.table_view.deselect_row()
+    item = all_items[section][cell_index]
+
+    view = ui.View()
+    view.background_color = ui.COLOR_SYSTEM_BACKGROUND
+
+    title_label = ui.Label(item.title)
+    title_label.font = ui.Font.bold_system_font_of_size(40)
+    title_label.size_to_fit()
+    title_label.text_alignment = ui.TEXT_ALIGNMENT_CENTER
+    title_label.width = view.width
+    title_label.flex = [ui.FLEXIBLE_WIDTH]
+    title_label.y = 30
+    view.add_subview(title_label)
+
+    parts = item.title.split()
+    url = "https://www.apple.com/"+parts[0].lower()
+    try:
+        url = url+"-"+parts[1].lower()
+    except IndexError:
+        pass
+
+    def show_more():
+        webbrowser.open(url)
+
+    button = ui.Button(title="Show more")
+    button.size_to_fit()
+    button.y = title_label.y+(button.width*2)
+    button.center_x = view.width/2
+    button.flex = [
+        ui.FLEXIBLE_BOTTOM_MARGIN,
+        ui.FLEXIBLE_TOP_MARGIN,
+        ui.FLEXIBLE_LEFT_MARGIN,
+        ui.FLEXIBLE_RIGHT_MARGIN
+    ]
+    button.action = show_more
+    view.add_subview(button)
   
-  view = ui.View()
-  view.background_color = ui.COLOR_SYSTEM_BACKGROUND
-  
-  title_label = ui.Label(item.title)
-  title_label.font = ui.Font.bold_system_font_of_size(40)
-  title_label.size_to_fit()
-  title_label.text_alignment = ui.TEXT_ALIGNMENT_CENTER
-  title_label.width = view.width
-  title_label.flex = [ui.FLEXIBLE_WIDTH]
-  title_label.y = 30
-  view.add_subview(title_label)
-  
-  parts = item.title.split()
-  url = "https://www.apple.com/"+parts[0].lower()
-  try:
-    url = url+"-"+parts[1].lower()
-  except IndexError:
-    pass
-  
-  def show_more():
-    webbrowser.open(url)
-  
-  button = ui.Button(title="Show more")
-  button.size_to_fit()
-  button.y = title_label.y+(button.width*2)
-  button.center_x = view.width/2
-  button.flex = [
-    ui.FLEXIBLE_BOTTOM_MARGIN,
-    ui.FLEXIBLE_TOP_MARGIN,
-    ui.FLEXIBLE_LEFT_MARGIN,
-    ui.FLEXIBLE_RIGHT_MARGIN
-  ]
-  button.action = show_more
-  view.add_subview(button)
-  
-  section.table_view.push(view)
-  
+    section.table_view.push(view)
+
 def deleted(section, cell_index):
-  all_items[section].pop(cell_index)
-  
+    all_items[section].pop(cell_index)
+
 def move(section, source_index, destination_index):
-  item = all_items[section].pop(source_index)
-  all_items[section].insert(destination_index, item)
+    item = all_items[section].pop(source_index)
+    all_items[section].insert(destination_index, item)
 
 for section in all_items.keys():
-  section.did_select_cell = selected
-  section.did_delete_cell = deleted
-  section.did_move_cell = move
+    section.did_select_cell = selected
+    section.did_delete_cell = deleted
+    section.did_move_cell = move
 
 table_view = ui.TableView(sections=all_items.keys())
 table_view.button_items = [table_view.edit_button_item]
