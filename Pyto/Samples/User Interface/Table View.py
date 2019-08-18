429fb4e573a5b40d1a2185220d1ca1e19d68e258
Adrian Labbé <adrilabbelol@gmail.com>
2019-08-18 19:52:27 -0400

    Fix Table View sample

--- Pyto/Samples/User Interface/Table View.py
+++ Pyto/Samples/User Interface/Table View.py
@@ -11,13 +11,13 @@ class Item:
         self.title = title
         self.subtitle = subtitle
 
-  def cell(self):
-      cell = ui.TableViewCell(ui.TABLE_VIEW_CELL_STYLE_SUBTITLE)
-      cell.text_label.text = self.title
-      cell.detail_text_label.text = self.subtitle
-      cell.accessory_type = ui.ACCESSORY_TYPE_DISCLOSURE_INDICATOR
-      cell.removable, cell.movable = True, True
-      return cell
+    def cell(self):
+        cell = ui.TableViewCell(ui.TABLE_VIEW_CELL_STYLE_SUBTITLE)
+        cell.text_label.text = self.title
+        cell.detail_text_label.text = self.subtitle
+        cell.accessory_type = ui.ACCESSORY_TYPE_DISCLOSURE_INDICATOR
+        cell.removable, cell.movable = True, True
+        return cell
 
 def cells(items):
     cells = []
