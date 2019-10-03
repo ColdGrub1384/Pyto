8e7e44bc22c2ed55c239760863866bd4943f5f13
Adrian Labbé <adrilabbelol@gmail.com>
2019-10-03 19:47:51 -0300

    Update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../Lib'))
 sys.path.insert(0, os.path.abspath('../site-packages'))
 
 # -- Project information -----------------------------------------------------
