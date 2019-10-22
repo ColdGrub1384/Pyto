db935c54b9b4c81638807a06b3f978242430c9f0
Adrian Labbé <adrilabbelol@gmail.com>
2019-10-22 18:36:02 -0300

    Update 'setup.sh'

--- setup.sh
+++ setup.sh
@@ -3,13 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR"
 
-# libffi
-
-cd libffi
-autoreconf -i
-python2 generate-darwin-source-and-headers.py --only-ios
-cd ../
-
 # Cocoapods and submodules
 
 pod install
