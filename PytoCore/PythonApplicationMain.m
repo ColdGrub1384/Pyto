b7dfa88fc869ed291a493223c20a89531ae6c583
Adrian Labbé <adrilabbelol@gmail.com>
2019-10-27 10:35:32 -0300

    Revert "Embed `_lzma` in main excecutable"
    This reverts commit 4083da0cc24b6fc02a2d4f725b0f2d3a3cc15f08.

--- PytoCore/PythonApplicationMain.m
+++ PytoCore/PythonApplicationMain.m
@@ -22,9 +22,8 @@ int PythonApplicationMain(NSString * scriptPath, int argc, char * _Nullable * _N
     putenv((char *)[[NSString stringWithFormat:@"PYTHONHOME=%@", bundle.bundlePath] UTF8String]);
     putenv((char *)[[NSString stringWithFormat:@"PYTHONPATH=%@:%@:%@:%@:%@:%@:%@:%@", [bundle.bundlePath stringByAppendingPathComponent:@"python38.zip"], [bundle.bundlePath stringByAppendingPathComponent:@"site-packages"], [bundle.bundlePath stringByAppendingPathComponent:@"Lib"], bundle.bundlePath, [NSBundle.mainBundle.bundlePath stringByAppendingString:@"site-packages"], [NSBundle.mainBundle.bundlePath stringByAppendingString:@"modules"], [NSBundle.mainBundle.bundlePath stringByAppendingString:@"iCloudDrive"], [NSBundle.mainBundle.bundlePath stringByAppendingString:@"Documents"]] UTF8String]);
     
-    // MARK: - Init extensions
+    // MARK: - Init PIL
     init_pil();
-    init__lzma();
     
     // MARK: - Init Python
     Py_SetPythonHome(Py_DecodeLocale([bundle.bundlePath UTF8String], NULL));
