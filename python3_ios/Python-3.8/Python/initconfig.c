fb4470dc2f722340567db9398a6f8ff476a80f9d
Adrian Labbé <adrilabbelol@gmail.com>
2019-10-29 12:04:38 -0300

    Set default encoding to UTF8

--- python3_ios/Python-3.8/Python/initconfig.c
+++ python3_ios/Python-3.8/Python/initconfig.c
@@ -109,7 +109,7 @@ static const char usage_6[] =
 
 /* UTF-8 mode (PEP 540): if equals to 1, use the UTF-8 encoding, and change
    stdin and stdout error handler to "surrogateescape". */
-int Py_UTF8Mode = 0;
+int Py_UTF8Mode = 1;
 int Py_DebugFlag = 0; /* Needed by parser.c */
 int Py_VerboseFlag = 0; /* Needed by import.c */
 int Py_QuietFlag = 0; /* Needed by sysmodule.c */
