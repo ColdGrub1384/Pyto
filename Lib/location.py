045715cb03484dc1e4c1697d319330118be50ef7
Adrian Labbé <adrilabbelol@gmail.com>
2020-01-05 19:52:51 -0300

    Fix #76

---
+++ Lib/location.py
@@ -0,0 +1,37 @@
+"""
+Accessing location
+
+This module gives access to the devices's location.
+"""
+
+import os
+import pyto
+from typing import Tuple
+
+if "widget" in os.environ:
+    raise ImportError("Cannot use this module on a widget.")
+
+__PyLocationHelper__ = pyto.PyLocationHelper
+
+def start_updating():
+    """
+    Starts receiving location updates. Call this before calling :func:`~location.get_location`.
+    """
+
+    __PyLocationHelper__.startUpdating()
+
+def stop_updating():
+    """
+    Stops receiving location updates.
+    """
+
+    __PyLocationHelper__.stopUpdating()
+
+def get_location() -> Tuple[float]:
+    """
+    Returns a tuple with current longitude, latitude and altitude.
+
+    :rtype: Tuple[float]
+    """
+
+    return (float(__PyLocationHelper__.longitude), float(__PyLocationHelper__.latitude), float(__PyLocationHelper__.altitude))
