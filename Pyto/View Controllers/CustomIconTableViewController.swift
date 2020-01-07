06f1d862910a751dea7660b1f675b1d31e4cef43
Adrian Labbé <adrilabbelol@gmail.com>
2020-01-07 17:07:44 -0300

    Add new alternate icons

--- Pyto/View Controllers/CustomIconTableViewController.swift
+++ Pyto/View Controllers/CustomIconTableViewController.swift
@@ -25,9 +25,15 @@ fileprivate extension Bundle {
 class CustomIconTableViewController: UITableViewController {
     
     /// A list of icons names.
-    let icons = ["Default", "Dark", "Light", "Stripes", "Type"]
+    var icons = ["Default", "Dark", "Light", "Stripes", "Type"]
     
     // MARK: - Custom icon table view controller
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidLoad()
+        
+        tableView.reloadData()
+    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return icons.count+1
@@ -40,13 +46,15 @@ class CustomIconTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "icon") ?? UITableViewCell(style: .default, reuseIdentifier: nil)
         cell.textLabel?.text = icons[indexPath.section]
-        if UIApplication.shared.alternateIconName == icons[indexPath.section] || UIApplication.shared.alternateIconName == nil && indexPath.section == 0 {
+        if UIApplication.shared.alternateIconName == icons[indexPath.section] || UIApplication.shared.alternateIconName == nil && icons[indexPath.section] == "Default" {
             cell.accessoryType = .checkmark
+        } else if (icons[indexPath.section] == "Dark" || icons[indexPath.section] == "Light") && icons.contains("Default") {
+            cell.accessoryType = .disclosureIndicator
         } else {
             cell.accessoryType = .none
         }
         
-        let icon = indexPath.section == 0 ? Bundle.main.icon : UIImage(named: icons[indexPath.section])
+        let icon = icons[indexPath.section] == "Default" ? Bundle.main.icon : UIImage(named: icons[indexPath.section])
         cell.imageView?.image = icon
         cell.imageView?.layer.cornerRadius = 16
         
@@ -64,7 +72,28 @@ class CustomIconTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        UIApplication.shared.setAlternateIconName(indexPath.section == 0 ? nil : icons[indexPath.section], completionHandler: { error in
+        let name: String?
+        if icons[indexPath.section] == "Default" {
+            name = nil
+        } else {
+            name = icons[indexPath.section]
+        }
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "icon") as? CustomIconTableViewController
+        var present = false
+        if name == "Dark" && icons.contains("Default") {
+            vc?.icons = ["Dark", "Dark 2", "Dark 3", "Dark 4", "Dark 5", "Dark 6", "Dark 7"]
+            present = true
+        } else if name == "Light" && icons.contains("Default") {
+            vc?.icons = ["Light", "Light 2", "Light 3", "Light 4", "Light 5", "Light 6"]
+            present = true
+        }
+        if present, let vc = vc {
+            navigationController?.pushViewController(vc, animated: true)
+            return
+        }
+        
+        UIApplication.shared.setAlternateIconName(name, completionHandler: { error in
             print(error?.localizedDescription ?? "No error")
         })
         
