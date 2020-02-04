beffff44b5d12b486bb0511aac2e8837b8ab54e7
Adrian Labbé <adrilabbelol@gmail.com>
2020-02-04 19:46:06 +0100

    Fix #129

--- Pyto/Samples/SciPy/ndimage.py
+++ Pyto/Samples/SciPy/ndimage.py
@@ -5,10 +5,9 @@ Taken from https://www.guru99.com/scipy-tutorial.html
 """
 
 from scipy import ndimage, misc
-from matplotlib import pyplot as plt
+from PIL import Image
 
 panda = misc.face()
 #rotatation function of scipy for image – image rotated 135 degree
 panda_rotate = ndimage.rotate(panda, 135)
-plt.imshow(panda_rotate)
-plt.show()
+Image.fromarray(panda_rotate).show()
