f36ef1c93d52d7534889295e0974d32a7c2460a7
Adrian Labbé <adrilabbelol@gmail.com>
2020-03-14 15:57:00 -0300

    Add 'speech' module

---
+++ Pyto/Python Bridging/PySpeech.swift
@@ -0,0 +1,49 @@
+//
+//  PySpeech.swift
+//  Pyto
+//
+//  Created by Adrian Labbé on 14-03-20.
+//  Copyright © 2020 Adrian Labbé. All rights reserved.
+//
+
+import AVFoundation
+
+@objc class PySpeech: NSObject {
+
+    static var speechSynthesizer = AVSpeechSynthesizer()
+    
+    @objc static func isSpeaking() -> Bool {
+        return speechSynthesizer.isSpeaking
+    }
+    
+    @objc static func wait() {
+        while isSpeaking() {
+            sleep(UInt32(0.2))
+        }
+    }
+    
+    @objc static func availableVoices() -> [String] {
+        var voices = [String]()
+        
+        for voice in AVSpeechSynthesisVoice.speechVoices() {
+            if !voices.contains(voice.language) {
+                voices.append(voice.language)
+            }
+        }
+        
+        return voices
+    }
+    
+    @objc static func say(_ text: String, language: String?, rate: Float) {
+        
+        let speechUtterance: AVSpeechUtterance = AVSpeechUtterance(string: text)
+        
+        speechUtterance.rate = rate
+        
+        if let lng = language {
+            speechUtterance.voice = AVSpeechSynthesisVoice(language: lng)
+        }
+                
+        speechSynthesizer.speak(speechUtterance)
+    }
+}
