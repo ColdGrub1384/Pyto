068abcec804f1daf42ecdb359d7b99c18bf452db
Adrian Labbé <adrilabbelol@gmail.com>
2020-05-22 18:03:34 -0400

    Localize SwiftUI views

--- SwiftUI Views/JSONBrowserView.swift
+++ SwiftUI Views/JSONBrowserView.swift
@@ -57,7 +57,7 @@ public struct JSONBrowserView: View {
     
     let dismiss: (() -> Void)?
     
-    init(title: String = "Inspector", dismiss: (() -> Void)? = nil, navBarMode: Binding<NavigationBarItem.TitleDisplayMode>, items: [String:Any], isArray: Bool = false) {
+    init(title: String = NSLocalizedString("inspector", bundle: SwiftUIBundle, comment: "The inspector title"), dismiss: (() -> Void)? = nil, navBarMode: Binding<NavigationBarItem.TitleDisplayMode>, items: [String:Any], isArray: Bool = false) {
         self.title = title
         self.dismiss = dismiss
         self._navBarMode = navBarMode
@@ -84,7 +84,7 @@ public struct JSONBrowserView: View {
                 self.dismiss?()
             }) {
                 if self.dismiss != nil {
-                    Text("Done").fontWeight(.bold)
+                    Text("done", bundle: SwiftUIBundle, comment: "Done button").fontWeight(.bold)
                 }
             }
         )
