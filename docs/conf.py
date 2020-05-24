6ff35ed4218398efbbffde1a2a4409869de4c6d4
Adrian Labbé <adrilabbelol@gmail.com>
2020-05-23 22:04:45 -0400

    Make the docs smaller and include them in the bundle

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../Lib'))
 sys.path.insert(0, os.path.abspath('../site-packages'))
+sys.path.insert(0, os.path.abspath('../downloadable-site-packages'))
 
 # -- Project information -----------------------------------------------------
 
