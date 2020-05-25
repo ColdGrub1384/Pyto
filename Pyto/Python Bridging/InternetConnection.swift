1fe10a10fcadf12ea6af39f13c360e9d47f9b683
Adrian Labbé <adrilabbelol@gmail.com>
2020-05-25 09:47:06 -0400

    Check for internet connection without reaching Google

---
+++ Pyto/Python Bridging/InternetConnection.swift
@@ -0,0 +1,61 @@
+//
+//  InternetConnection.swift
+//  Pyto
+//
+//  Created by Adrian Labbé on 25-05-20.
+//  Copyright © 2020 Adrian Labbé. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+@objc class InternetConnection: NSObject {
+    enum ReachabilityStatus {
+        case notReachable
+        case reachableViaWWAN
+        case reachableViaWiFi
+    }
+    
+    @objc static var isConnectedToInternet: Bool {
+        return currentReachabilityStatus != .notReachable
+    }
+    
+    static var currentReachabilityStatus: ReachabilityStatus {
+
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return .notReachable
+        }
+
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return .notReachable
+        }
+
+        if flags.contains(.reachable) == false {
+            // The target host is not reachable.
+            return .notReachable
+        }
+        else if flags.contains(.isWWAN) == true {
+            // WWAN connections are OK if the calling application is using the CFNetwork APIs.
+            return .reachableViaWWAN
+        }
+        else if flags.contains(.connectionRequired) == false {
+            // If the target host is reachable and no connection is required then we'll assume that you're on Wi-Fi...
+            return .reachableViaWiFi
+        }
+        else if (flags.contains(.connectionOnDemand) == true || flags.contains(.connectionOnTraffic) == true) && flags.contains(.interventionRequired) == false {
+            // The connection is on-demand (or on-traffic) if the calling application is using the CFSocketStream or higher APIs and no [user] intervention is needed
+            return .reachableViaWiFi
+        }
+        else {
+            return .notReachable
+        }
+    }
+}
