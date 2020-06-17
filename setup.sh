1eec1dbd08c758db548d4c027f9c9c5d69aa2de4
Adrian Labbé <adrilabbelol@gmail.com>
2020-06-17 12:31:15 -0400

    Fix `setup.sh`

--- setup.sh
+++ setup.sh
@@ -5,14 +5,10 @@ cd "$DIR"
 
 # OpenCV
 
-cd OpenCV
-
 curl -L -O https://github.com/ColdGrub1384/OpenCV-Contrib-iOS/releases/download/4.3.0/opencv2.framework.zip
 unzip opencv2.framework.zip
 rm -rf opencv2.framework.zip
 rm -rf __MACOSX
-
-cd ../
 
 # Cocoapods and submodules
 
