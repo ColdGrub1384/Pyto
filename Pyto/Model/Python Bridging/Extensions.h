59d3d23df50a28296f161f33ecb065bb133a4284
Adrian Labbé <adrilabbelol@gmail.com>
2020-07-04 00:49:19 -0400

    Fix extensions on `Release` config

---
+++ Pyto/Model/Python Bridging/Extensions.h
@@ -0,0 +1,32 @@
+//
+//  Extensions.h
+//  Pyto
+//
+//  Created by Adrian Labbé on 03-07-20.
+//  Copyright © 2020 Adrian Labbé. All rights reserved.
+//
+
+#if !(TARGET_IPHONE_SIMULATOR)
+#if MAIN
+void init_lxml(void);
+void init_scipy(void);
+void init_sklearn(void);
+void init_skimage(void);
+void init_pil(void);
+void init_numpy(void);
+void init_cffi(void);
+void init_bcrypt(void);
+void init_pywt(void);
+void init_statsmodels(void);
+void init_zmq(void);
+void init_gensim(void);
+void init_regex(void);
+void init_astropy(void);
+void init_emd(void);
+void init_cv2(void);
+void init_nacl(void);
+void init_matplotlib(void);
+void init_pandas(void);
+void init_biopython(void);
+#endif
+#endif
---
+++ libfreetype/libfreetype.h
@@ -0,0 +1,19 @@
+//
+//  libfreetype.h
+//  libfreetype
+//
+//  Created by Adrian Labbé on 03-07-20.
+//  Copyright © 2020 Adrian Labbé. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libfreetype.
+FOUNDATION_EXPORT double libfreetypeVersionNumber;
+
+//! Project version string for libfreetype.
+FOUNDATION_EXPORT const unsigned char libfreetypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libfreetype/PublicHeader.h>
+
+
