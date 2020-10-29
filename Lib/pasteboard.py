cda2edf383b21e6869197c77da94136439762d79
Adrian Labbé <adrilabbelol@gmail.com>
2020-10-29 15:42:59 -0300

    Use standard types on the `pasteboard` module

--- Lib/pasteboard.py
+++ Lib/pasteboard.py
@@ -5,9 +5,12 @@ Pasteboard Access
 This module gives access to the pasteboard.
 """
 
-from UIKit import UIPasteboard as __UIPasteboard__, UIImage
+from UIKit import UIPasteboard
+from Foundation import NSURL
 from typing import List
 from __check_type__ import check
+from __image__ import __ui_image_from_pil_image__, __pil_image_from_ui_image__
+from PIL import Image
 
 try:
     from rubicon.objc import ObjCClass, ObjCInstance
@@ -21,7 +24,7 @@ NSURL = ObjCClass("NSURL")
 
 
 def general_pasteboard():
-    return __UIPasteboard__.generalPasteboard
+    return UIPasteboard.generalPasteboard
 
 
 # MARK: - Text
@@ -68,33 +71,46 @@ def set_strings(array: List[str]):
 # MARK: - Images
 
 
-def image() -> UIImage:
+def image() -> Image.Image:
     """
-    Returns the image contained in the pasteboard as an Objective-C ``UIImage``.
+    Returns the image contained in the pasteboard as PIL images.
     """
-    return general_pasteboard().image
+
+    image = general_pasteboard().image
+    if image is None:
+        return None
+    return __pil_image_from_ui_image__(image)
 
 
-def images() -> List[UIImage]:
+def images() -> List[Image.Image]:
     """
-    Returns all images contained in the pasteboard as Objective-C ``UIImage`` s.
+    Returns all images contained in the pasteboard as PIL images.
     """
-    return general_pasteboard().images
+
+    images = general_pasteboard().images
+    if images is None:
+        return []
+
+    pil_images = []
+    for image in images:
+        pil_images.append(__pil_image_from_ui_image__(image))
+
+    return pil_images
 
 
-def set_image(image: ObjCInstance):
+def set_image(image: Image.Image):
     """
     Copy the given image to the pasteboard.
 
     :param image: The image to copy.
     """
 
-    check(image, "image", [ObjCInstance, None])
+    check(image, "image", [Image.Image, None])
 
-    general_pasteboard().image = image
+    general_pasteboard().image = __ui_image_from_pil_image__(image)
 
 
-def set_images(array: List[UIImage]):
+def set_images(array: List[Image.Image]):
     """
     Copy the given images to the pasteboard.
 
@@ -103,45 +119,66 @@ def set_images(array: List[UIImage]):
 
     check(array, "array", [list, None])
 
-    general_pasteboard().images = array
+    images = []
+    if array is not None:
+        for image in array:
+            images.append(__ui_image_from_pil_image__(image))
+
+    general_pasteboard().images = images
 
 
 # MARK: - URLs
 
 
-def url() -> NSURL:
+def url() -> str:
     """
-    Returns the URL contained in the pasteboard as an Objective-C ``NSURL``.
+    Returns the URL contained in the pasteboard as a string.
     """
-    return general_pasteboard().url
+    url = general_pasteboard().URL
+    if url is not None:
+        url = str(url.absoluteString)
+    return url
 
 
-def urls() -> List[NSURL]:
+def urls() -> List[str]:
     """
-    Returns all URLs contained in the pasteboard as Objective-C ``NSURL`` s.
+    Returns all URLs contained in the pasteboard as strings.
     """
-    return general_pasteboard().urls
+
+    urls = general_pasteboard().URLs
+    str_urls = []
+
+    if urls is not None:
+        for url in urls:
+            str_urls.append(str(url.absoluteString))
+
+    return str_urls
 
 
-def set_url(url: NSURL):
+def set_url(url: str):
     """
     Copy the given URL to the pasteboard.
 
-    :param url: The Objective-C ``NSURL`` to copy.
+    :param url: The string to copy as an URL.
     """
 
-    check(url, "url", [ObjCInstance, None])
+    check(url, "url", [str, None])
 
-    general_pasteboard().url = url
+    general_pasteboard().URL = NSURL.alloc().initWithString(url)
 
 
-def set_urls(array: List[NSURL]):
+def set_urls(array: List[str]):
     """
     Copy the given URLs to the pasteboard.
 
-    :param array: A list of Objective-C ``NSURL`` s to copy.
+    :param array: A list of strings to copy as URLs.
     """
 
     check(array, "array", [list, None])
 
-    general_pasteboard().urls = array
+    urls = []
+    if array is not None:
+        for url in array:
+            urls.append(NSURL.alloc().initWithString(url))
+
+    general_pasteboard().URLs = urls
