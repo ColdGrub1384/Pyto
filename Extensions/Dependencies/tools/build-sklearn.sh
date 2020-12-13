ea09c24e651a90ea35335ffc572cfccc7ffeb6c5
Adrian Labbé <adrilabbelol@gmail.com>
2020-12-13 12:58:17 -0300

    Fix broken libraries

--- Extensions/Dependencies/tools/build-sklearn.sh
+++ Extensions/Dependencies/tools/build-sklearn.sh
@@ -12,3 +12,7 @@ cd ../sklearn
 python3 setup.py bdist
 python3 ../tools/make_frameworks.py sklearn SkLearn
 ../tools/copy-scripts.sh build/lib*/* ../../../downloadable-site-packages/compiled/sklearn
+
+cp -r sklearn/datasets/data ../../../downloadable-site-packages/compiled/sklearn/datasets
+cp -r sklearn/datasets/descr ../../../downloadable-site-packages/compiled/sklearn/datasets
+cp -r sklearn/datasets/images ../../../downloadable-site-packages/compiled/sklearn/datasets
